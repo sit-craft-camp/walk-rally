c04118aeef46ace32fd803b347a9887d06427a46
Kanisorn S <firstziiz.k@gmail.com>
2017-10-21 12:54:53 +0700

    remove key

--- src/firebaseKey.js
+++
@@ -1,8 +0,0 @@
-export default {
-  apiKey: 'AIzaSyBFAXmjPh48ztxxqfQ1V0Jgwq7zPEAhgec',
-  authDomain: 'sit-craft-rally.firebaseapp.com',
-  databaseURL: 'https://sit-craft-rally.firebaseio.com',
-  projectId: 'sit-craft-rally',
-  storageBucket: 'sit-craft-rally.appspot.com',
-  messagingSenderId: '920710860721'
-}
